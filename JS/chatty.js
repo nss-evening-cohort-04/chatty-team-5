1fe6eb11dc8c4b3ab020b409d9cbd9814842e7c6
Stephen Szpak <srszpak@gmail.com>
2016-09-15 17:41:42 -0500

    super

--- JS/chatty.js
+++ JS/chatty.js
@@ -9,16 +9,27 @@ var Chatty = (function () {
       
     },
     loadFixedMessages: function() {
-    var messageLoader = new XMLHttpRequest();
-    messageLoader.addEventListener("load", function() {
-    fixedMessages = JSON.parse(this.responseText);
-    fixedMessages = fixedMessages.messages;
-    printMessages(fixedMessages);
+      var loadFile = function (filePath, done) {
+      var xhr = new XMLHTTPRequest();
+      xhr.onload = function () { return done(this.responseText) }
+      xhr.open("GET", filePath, true);
+      xhr.send();
+        };
 
-    });
-    messageLoader.open("GET", "storedMessages.JSON");
-    messageLoader.send();
+    var myFiles = [ "json/msg-one.json", "json/msg-two.json", "json/msg-three.json", "json/msg-four.json", "json/msg-five.json" ];
 
+    myFiles.forEach(function (file, i) {
+    loadFile(file, function (responseText) {
+        // we set jsonData[i] to the parse data since the requests
+        // will not necessarily come in order
+        // so we can't use JSONdata.push(JSON.parse(responseText));
+        // if the order doesn't matter, you can use push
+        fixedMessages[i] = JSON.parse(responseText);
+        // or you could choose not to store it in an array.
+        // whatever you decide to do with it, it is available as
+        // responseText within this scope (unparsed!)
+    })
+})
     },
     eraseMessages: function(msgId) {
         // if (msgId === undefined) {
